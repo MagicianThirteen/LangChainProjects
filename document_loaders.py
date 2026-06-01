80024d482d00945f67fd2cd24459eb2b33bcad02
zhangyuan <ZARLZYJILL@163.com>
2026-06-01 20:06:05 +0800

    网页加载器

--- document_loaders.py
+++ document_loaders.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv
 from langchain_community.document_loaders import(
-    TextLoader
+    TextLoader,
+    WebBaseLoader
 )
 import os
 import tempfile
+from bs4 import BeautifulSoup
 
 load_dotenv()
+
+#使用txtloader
 def load_text_file():
     with tempfile.NamedTemporaryFile(
         delete=False,
@@ -33,7 +37,41 @@ def load_text_file():
         
     finally:
         os.remove(temp_file_path)
+    
+#使用网页加载器
+def web_loader():
+    loader=WebBaseLoader(
+        "https://www.baidu.com/"
+    )
+    documents=loader.load()
+
+    print(f"Loaded{len(documents)} document(s) from baidu")
+    print(f"source:{documents[0].metadata.get('source','N/A')}")
+    print(f"Length:{documents[0].page_content} 字数")
+    print(f"{documents[0].page_content[:10]}……")
+    '''
+    输出
+        Loaded1 document(s) from baidu
+    source:https://www.baidu.com/
+    Length:
+
+
+
+
+
+
+    字数
+
+
+
+
+
+
+
+    ……
+    '''
 
 
 if __name__ == "__main__":
-    load_text_file()
+    #load_text_file()
+    web_loader()
